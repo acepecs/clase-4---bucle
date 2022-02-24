3d071dc39117250eebebe6dd7436d6b36810d6b7
martin.acerenza <acerenzamartin@gmail.com>
2022-02-23 21:16:36 -0300

    bucle

---
+++ main.js
@@ -0,0 +1,12 @@
+
+
+const ProyectosFinales = prompt ("cual fue tu nota?");
+
+if ( ProyectosFinales < 6) {
+    console.log("desaprobaste");
+} else if ( ProyectosFinales == 8) {
+    console.log ("tuviste un buen promedio")
+} else {
+    console.log ("aprobaste locoo")
+}
+
